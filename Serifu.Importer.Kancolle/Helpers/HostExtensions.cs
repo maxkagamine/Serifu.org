630cf6c6e6da879af3d2521002852f02a9fb7ce3
Max Kagamine <max@kgmn.jp>
2023-10-31 14:08:03 +0900

    Created an AddEntryPoint() as an alternative to AddHostedService() that's better-suited towards console apps

---
+++ Serifu.Importer.Kancolle/Helpers/HostExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Extensions.DependencyInjection;
+internal static class HostExtensions
+{
+    /// <summary>
+    /// Adds <typeparamref name="T"/> as a scoped service and registers an <see cref="IHostedService"/> which will
+    /// execute it using the entrypoint defined by <paramref name="main"/> and exit upon completion.
+    /// </summary>
+    /// <typeparam name="T">The entrypoint class.</typeparam>
+    /// <param name="services">The service collection.</param>
+    /// <param name="main">
+    /// A function that calls the entrypoint method on <typeparamref name="T"/> and returns a <see cref="Task"/>. The
+    /// function is passed a <see cref="CancellationToken"/> that will be triggered if the application is stopped e.g.
+    /// due to a Ctrl+C interrupt.
+    /// </param>
+    public static IServiceCollection AddEntryPoint<T>(
+        this IServiceCollection services,
+        Func<T, CancellationToken, Task<int>> main) where T : class
+    {
+        services.AddScoped<T>();
+        return services.AddHostedService<DelegateService>(provider => new(async stoppingToken =>
+        {
+            using var scope = provider.CreateScope();
+            var entrypoint = scope.ServiceProvider.GetRequiredService<T>();
+            int exitCode = await main(entrypoint, stoppingToken);
+            Environment.Exit(exitCode);
+        }));
+    }
+
+    /// <inheritdoc cref="AddEntryPoint{T}(IServiceCollection, Func{T, CancellationToken, Task{Int32}})"/>
+    public static IServiceCollection AddEntryPoint<T>(
+        this IServiceCollection services,
+        Func<T, CancellationToken, Task> main) where T : class
+        => services.AddEntryPoint<T>(async (entry, stoppingToken) =>
+        {
+            await main(entry, stoppingToken);
+            return 0;
+        });
+
+    private class DelegateService : BackgroundService
+    {
+        private readonly Func<CancellationToken, Task> executeAsync;
+
+        public DelegateService(Func<CancellationToken, Task> executeAsync)
+        {
+            this.executeAsync = executeAsync;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken) => executeAsync(stoppingToken);
+    }
+}
--- Serifu.Importer.Kancolle/KancolleImporter.cs
+++ Serifu.Importer.Kancolle/KancolleImporter.cs
@@ -1,13 +1,12 @@
 ﻿using System.Diagnostics;
 using System.Net;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serifu.Data.Entities;
 using Serifu.Importer.Kancolle.Models;
 using Serifu.Importer.Kancolle.Services;
 
 namespace Serifu.Importer.Kancolle;
-internal class KancolleImporter : BackgroundService
+internal class KancolleImporter
 {
     private readonly ShipListService shipListService;
     private readonly ShipService shipService;
@@ -29,22 +28,21 @@ internal class KancolleImporter : BackgroundService
         this.logger = logger;
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public async Task Import(CancellationToken cancellationToken)
     {
         var sw = Stopwatch.StartNew();
 
         await quotesService.Initialize();
 
-        var ships = await shipListService.GetShips(stoppingToken);
+        var ships = await shipListService.GetShips(cancellationToken);
 
         foreach (Ship ship in ships)
         {
-            await ImportShip(ship, stoppingToken);
+            await ImportShip(ship, cancellationToken);
         }
 
         sw.Stop();
         logger.LogInformation("Import completed in {Duration}.", sw.Elapsed);
-        Environment.Exit(0);
     }
 
     private async Task ImportShip(Ship ship, CancellationToken cancellationToken = default)
