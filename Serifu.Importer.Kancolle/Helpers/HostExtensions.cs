4c203832d64b414dc7170969d8c506453de321ec
Max Kagamine <max@kgmn.jp>
2023-11-01 12:10:11 +0900

    Implement better unhandled exception handling

--- Serifu.Importer.Kancolle/Helpers/HostExtensions.cs
+++ Serifu.Importer.Kancolle/Helpers/HostExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Microsoft.Extensions.DependencyInjection;
 internal static class HostExtensions
@@ -24,7 +25,19 @@ internal static class HostExtensions
             using var scope = provider.CreateScope();
             var entrypoint = scope.ServiceProvider.GetRequiredService<T>();
             var lifetime = scope.ServiceProvider.GetRequiredService<IHostApplicationLifetime>();
-            Environment.ExitCode = await main(entrypoint, stoppingToken);
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger>();
+
+            try
+            {
+                Environment.ExitCode = await main(entrypoint, stoppingToken);
+            }
+            catch (Exception ex) when (ex is not OperationCanceledException)
+            {
+                // Microsoft.Extensions.Hosting prints exceptions twice and exits with 0... we'll do it ourselves.
+                logger.Fatal(ex, "Unhandled exception.");
+                Environment.ExitCode = 255;
+            }
+
             lifetime.StopApplication();
         }));
     }
