7cd26cb4372ba9228ed3912b7a35d02f9cf55a5f
Max Kagamine <max@kgmn.jp>
2023-11-03 04:27:22 +0900

    Handle duplicates

--- Serifu.Importer.Kancolle/Services/ShipService.cs
+++ Serifu.Importer.Kancolle/Services/ShipService.cs
@@ -65,6 +65,34 @@ internal partial class ShipService
                 SortOrder = i++,
             };
 
+            if (quotes.Any(q =>
+                q.Context == quote.Context &&
+                q.QuoteEnglish == quote.QuoteEnglish &&
+                q.QuoteJapanese == quote.QuoteJapanese &&
+                q.AudioFile == quote.AudioFile))
+            {
+                // Kasuga Maru has a separate table for her Taiyou remodel with many of the same lines duplicated.
+                continue;
+
+                /* Regarding duplicates, here's my truth table for different cases after analyzing the data (x = same):
+                 *
+                 * Ctx Jpn Eng Aud
+                 *  x   x   x   x   Duplicate (Kasuga Maru only)
+                 *  ?   x   x       Sometimes the same line is spoken with different intonation. (There are also some
+                 *                  identical audio files; possibly others that are the same too but not byte-for-byte.)
+                 *  ?   x       x   Different translations for the same voice line. Only 9 such cases; may as well keep.
+                 *  ?   x           Combination of the above two cases.
+                 *  ?       ?   x   ⚠️ Could be different punctuation, kanji vs. kana, etc., but could also be a mistake
+                 *                  in the wiki (copy-paste error). Log a warning.
+                 *  ?       x       Different voice lines that coincidentally have the same translation.
+                 *  x               This happens a lot, either due to seasonal quotes putting the specific context in
+                 *                  the notes or because the line is for a remodel which was noted elsewhere.
+                 *      x   x   x   Same voice line used in different contexts or reused for events. Surprisingly only
+                 *                  four, but could be more if we identify copies of the same audio. Could concat the
+                 *                  contexts, but since there's so few we may as well leave them as-is.
+                 */
+            }
+
             quotes.Add(quote);
         }
 
@@ -77,15 +105,13 @@ internal partial class ShipService
             logger.Information("Found {Count} quotes for {Ship}.", quotes.Count, ship);
         }
 
-        var duplicateAudioFiles = quotes
+        foreach (var audioFile in quotes
             .GroupBy(q => q.AudioFile)
-            .Where(g => g.Key is not null && g.Count() > 1)
-            .Select(g => g.Key!)
-            .ToArray();
-        if (duplicateAudioFiles.Length > 0)
+            .Where(g => g.Key is not null && g.DistinctBy(q => q.QuoteJapanese).Count() > 1)
+            .Select(g => g.Key!))
         {
-            logger.Warning("{Ship}'s quotes contain the following audio files more than once: {AudioFiles}. This is probably a copy-paste error.",
-                ship, duplicateAudioFiles);
+            logger.Warning("{Ship} has voice lines with different Japanese but the same audio file {AudioFile}.",
+                ship, audioFile);
         }
 
         return quotes;
