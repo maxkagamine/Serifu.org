f9f5c192c817fb69d426d9bcb2e04093c6b6d8a9
Max Kagamine <max@kgmn.jp>
2023-11-28 12:44:14 +0900

    Remove context numberings since we won't be displaying them in order

--- Serifu.Importer.Kancolle/Helpers/Regexes.cs
+++ Serifu.Importer.Kancolle/Helpers/Regexes.cs
@@ -16,13 +16,13 @@ internal static partial class Regexes
     private static partial Regex SlashRegex();
 
     [GeneratedRegex(@"(?<=[a-z])[A-Z0-9]")]
-    private static partial Regex PascalCasedLettersRegex();
+    private static partial Regex FirstCharacterOfPascalCaseWordRegex();
 
     [GeneratedRegex(@"^.|(?<=[ ([]).")]
     private static partial Regex FirstCharacterOfWordRegex();
 
     [GeneratedRegex(@" (a|an|and|as|at|but|by|for|if|in|nor|of|off|on|or|per|so|the|to|up|via|yet)(?= )", RegexOptions.IgnoreCase)]
-    private static partial Regex LowercaseWordsRegex();
+    private static partial Regex TitleCaseLowercaseWordsRegex();
 
     [GeneratedRegex(@"Docking \(?(Minor|Major)(?: Damage)?\)?")]
     private static partial Regex DockingMajorMinorDamageRegex();
@@ -32,6 +32,12 @@ internal static partial class Regexes
 
     [GeneratedRegex(@"(?<=(st|nd|rd|th) Anniversary) \d{4}")]
     private static partial Regex YearNextToAnniversaryRegex();
+
+    [GeneratedRegex(@"^Special(?! ?[A-Za-z])")]
+    private static partial Regex JustSpecialRegex();
+
+    [GeneratedRegex(@"(?<!\(.*)\s\d\b")]
+    private static partial Regex SingleDigitNumberAfterContextRegex();
 
     /// <inheritdoc cref="ContextTokenizerRegex"/>
     public static Regex ContextTokenizer => ContextTokenizerRegex();
@@ -45,14 +51,14 @@ internal static partial class Regexes
     /// <inheritdoc cref="SlashRegex"/>
     public static Regex Slash => SlashRegex();
 
-    /// <inheritdoc cref="PascalCasedLettersRegex"/>
-    public static Regex PascalCasedLetters => PascalCasedLettersRegex();
+    /// <inheritdoc cref="FirstCharacterOfPascalCaseWordRegex"/>
+    public static Regex FirstCharacterOfPascalCaseWord => FirstCharacterOfPascalCaseWordRegex();
 
     /// <inheritdoc cref="FirstCharacterOfWordRegex"/>
     public static Regex FirstCharacterOfWord => FirstCharacterOfWordRegex();
 
-    /// <inheritdoc cref="LowercaseWordsRegex"/>
-    public static Regex LowercaseWords => LowercaseWordsRegex();
+    /// <inheritdoc cref="TitleCaseLowercaseWordsRegex"/>
+    public static Regex TitleCaseLowercaseWords => TitleCaseLowercaseWordsRegex();
 
     /// <inheritdoc cref="DockingMajorMinorDamageRegex"/>
     public static Regex DockingMajorMinorDamage => DockingMajorMinorDamageRegex();
@@ -62,4 +68,10 @@ internal static partial class Regexes
 
     /// <inheritdoc cref="YearNextToAnniversaryRegex"/>
     public static Regex YearNextToAnniversary => YearNextToAnniversaryRegex();
+
+    /// <inheritdoc cref="JustSpecialRegex"/>
+    public static Regex JustSpecial => JustSpecialRegex();
+
+    /// <inheritdoc cref="SingleDigitNumberAfterContextRegex"/>
+    public static Regex SingleDigitNumberAfterContext => SingleDigitNumberAfterContextRegex();
 }
