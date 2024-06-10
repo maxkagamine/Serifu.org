bade938b6c230dd7a97a7452e3d3ed409d0cd29a
Max Kagamine <max@kgmn.jp>
2024-06-10 20:26:32 +0900

    Replaced some of the helpers with Kagamine.Extensions

--- Serifu.Importer.Kancolle/Program.cs
+++ Serifu.Importer.Kancolle/Program.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Kagamine.Extensions.Hosting;
+using Kagamine.Extensions.Logging;
+using Kagamine.Extensions.Utilities;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using Serifu.Importer.Kancolle;
+using Serifu.Data;
+using Serifu.Data.Local;
 using Serifu.Importer.Kancolle.Helpers;
 using Serifu.Importer.Kancolle.Services;
 using Serilog;
 using Serilog.Events;
 
-var builder = Host.CreateApplicationBuilder();
+var builder = ConsoleApplication.CreateBuilder();
 
 builder.Services.AddSerilog(config => config
     .MinimumLevel.Debug()
@@ -26,7 +30,35 @@ builder.Services.AddScoped<ShipListService>();
 builder.Services.AddScoped<ShipService>();
 builder.Services.AddScoped<WikiApiService>();
 
-builder.Services.AddEntryPoint<KancolleImporter>((importer, cancellationToken) =>
-    importer.Import(cancellationToken));
+builder.Run(async (
+    ShipListService shipListService,
+    ShipService shipService,
+    ILocalDataService localDataService,
+    ILogger logger,
+    CancellationToken cancellationToken) =>
+{
+    Console.Title = "Kancolle Importer";
 
-await builder.Build().RunAsync();
+    await localDataService.Initialize();
+
+    using (logger.BeginTimedOperation("Import"))
+    using (var progress = new TerminalProgressBar())
+    {
+        List<Quote> quotes = [];
+        var ships = (await shipListService.GetShips(cancellationToken)).ToList();
+
+        for (int i = 0; i < ships.Count; i++)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            progress.SetProgress(i, ships.Count);
+
+            var shipQuotes = await shipService.GetQuotes(ships[i], cancellationToken);
+
+            quotes.AddRange(shipQuotes);
+        }
+
+        await localDataService.ReplaceQuotes(Source.Kancolle, quotes, cancellationToken);
+    }
+
+    await localDataService.DeleteOrphanedAudioFiles(cancellationToken);
+});
