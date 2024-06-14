43169b4cf6ca965dcd6d32bd6211bcf206a5f17c
Max Kagamine <max@kgmn.jp>
2024-06-15 07:40:20 +0900

    Log to Seq

--- Serifu.Data.Sqlite/DependencyInjectionExtensions.cs
+++ Serifu.Data.Sqlite/DependencyInjectionExtensions.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Filters;
 
 namespace Serifu.Data.Sqlite;
 
@@ -11,4 +16,35 @@ public static class DependencyInjectionExtensions
 
         return services;
     }
+
+    // Not strictly "data" but it's convenient here since all importers will reference this project
+    public static IServiceCollection AddSerifuSerilog(
+        this IServiceCollection services,
+        Action<LoggerConfiguration>? configureLogger = null,
+        Action<LoggerConfiguration>? configureConsoleLogger = null)
+        => services.AddSerilog((IServiceProvider provider, LoggerConfiguration config) =>
+        {
+            string appName = provider.GetRequiredService<IHostEnvironment>().ApplicationName;
+
+            string seqUrl = provider.GetRequiredService<IConfiguration>()["SeqUrl"] ??
+                throw new Exception("SeqUrl not set in configuration (user secrets)");
+
+            config
+                .MinimumLevel.Debug()
+                .Enrich.WithProperty("Application", appName)
+                .Enrich.WithProperty("InvocationId", Guid.NewGuid())
+                .WriteTo.Logger(consoleLogger =>
+                {
+                    consoleLogger
+                        .Filter.ByExcluding(logEvent => logEvent.Level < LogEventLevel.Warning && (
+                            Matching.FromSource("System")(logEvent) ||
+                            Matching.FromSource("Microsoft")(logEvent)))
+                        .WriteTo.Console();
+
+                    configureConsoleLogger?.Invoke(config);
+                })
+                .WriteTo.Seq(seqUrl);
+
+            configureLogger?.Invoke(config);
+        });
 }
