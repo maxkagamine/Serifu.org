b45108eeb91a4727d1e9502b072982ada5ebb39c
Max Kagamine <max@kgmn.jp>
2024-06-17 11:21:45 +0900

    Stubbed out elasticsearch projects

---
+++ Serifu.Data.Elasticsearch.Build/Program.cs
@@ -0,0 +1,40 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Kagamine.Extensions.Hosting;
+using Serifu.Data.Elasticsearch;
+using Serifu.Data.Sqlite;
+using Serilog;
+using Serilog.Events;
+
+var builder = ConsoleApplication.CreateBuilder();
+
+builder.Services.AddSerilog(config => config
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .WriteTo.Console(outputTemplate: "{Message:lj}{NewLine}{Exception}"));
+
+builder.Services.AddSerifuElasticsearch();
+builder.Services.AddSerifuSqlite();
+
+builder.Run(async (
+    ElasticsearchClient elasticsearch,
+    SerifuDbContext sqlite,
+    ILogger logger,
+    CancellationToken cancellationToken) =>
+{
+    logger.Information("Waiting for elasticsearch to start...");
+
+    while (true)
+    {
+        try
+        {
+            await elasticsearch.PingAsync(cancellationToken);
+            break;
+        }
+        catch
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+        }
+    }
+
+    logger.Information("Ready.");
+});
---
+++ Serifu.Data.Elasticsearch/DependencyInjectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Serifu.Data.Elasticsearch;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddSerifuElasticsearch(this IServiceCollection services)
+    {
+        // TODO: ES server defaults to localhost:9200, should get from configuration
+        services.AddSingleton<IElasticsearchClientSettings>(new ElasticsearchClientSettings()
+#if DEBUG
+            .EnableDebugMode()
+#endif
+            .DefaultIndex("quotes")
+            .ThrowExceptions());
+
+        services.AddSingleton<ElasticsearchClient>();
+
+        return services;
+    }
+}
