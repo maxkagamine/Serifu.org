cef3b82fc489ba63edf9416d4a07bce0068e5d4c
Max Kagamine <max@kgmn.jp>
2024-07-02 05:03:14 +0900

    Japanese tokenizer

--- Serifu.ML.Tests/TokenizerTests.cs
+++ Serifu.ML.Tests/TokenizerTests.cs
@@ -1,4 +1,5 @@
-﻿using Serifu.ML.Tokenizers;
+﻿using Serifu.ML.Abstractions;
+using Serifu.ML.Tokenizers;
 
 namespace Serifu.ML.Tests;
 
@@ -9,10 +10,19 @@ public class TokenizerTests
     [InlineData("CarDiv3, flagship Zuihou, setting sail!", new[] { "CarDiv", "3", "flagship", "Zuihou", "setting", "sail" })]
     [InlineData("I'm Hibi-- Verniy. It's a name that means \"Faithful\".", new[] { "I'm", "Hibi", "Verniy", "It's", "a", "name", "that", "means", "Faithful" })]
     [InlineData("Foo’s 'wouldn't've' this/that; sueño $9,000.00.", new[] { "Foo’s", "wouldn't've", "this", "that", "sueño", "9,000.00" })]
-    public void EnglishTokenizer(string text, string[] expected)
-    {
-        EnglishTokenizer tokenizer = new();
+    public void EnglishTokenizer(string text, string[] expected) =>
+        Tokenizer(new EnglishTokenizer(), text, expected);
 
+    [Theory]
+    [InlineData("軽巡、多摩です。猫じゃないにゃ。", new[] { "軽巡", "多摩", "です", "猫", "じゃない", "にゃ" })]
+    [InlineData("瑞鳳です。軽空母ですが、練度が上がれば、正規空母並の活躍をお見せできます。", new[] { "瑞鳳", "です", "軽", "空母", "です", "が", "練度", "が", "上がれば", "正規", "空母", "並", "の", "活躍", "を", "お", "見せできます" })]
+    [InlineData("ひび…Верный だ。信頼できると言う意味の名なんだ", new[] { "ひび", "Верный", "だ", "信頼", "できる", "と", "言う", "意味", "の", "名", "な", "ん", "だ" })]
+    [InlineData("彗星は彗星で悪くないんだけれど、整備大変なのよ、整備が。", new[] { "彗星", "は", "彗星", "で", "悪くない", "ん", "だ", "けれど", "整備", "大変", "な", "の", "よ", "整備", "が" })]
+    public void JapaneseTokenizer(string text, string[] expected) =>
+        Tokenizer(new JapaneseTokenizer(), text, expected);
+
+    private static void Tokenizer(ITokenizer tokenizer, string text, string[] expected)
+    {
         Range[] ranges = tokenizer.Tokenize(text).ToArray();
         string[] actual = ranges.Select(r => text[r]).ToArray();
 
---
+++ Serifu.ML/Tokenizers/JapaneseTokenizer.cs
@@ -0,0 +1,40 @@
+﻿using MeCab;
+using MeCab.Extension.IpaDic;
+using Serifu.ML.Abstractions;
+using Ve.DotNet;
+
+namespace Serifu.ML.Tokenizers;
+
+public sealed class JapaneseTokenizer : ITokenizer, IDisposable
+{
+    private readonly MeCabTagger mecab = MeCabTagger.Create();
+
+    public IEnumerable<Range> Tokenize(string text)
+    {
+        List<Range> ranges = [];
+
+        IEnumerable<MeCabNode> nodes = mecab.ParseToNodes(text);
+        IEnumerable<VeWord> words = nodes.ParseVeWords();
+
+        int cursor = 0;
+
+        foreach (VeWord word in words)
+        {
+            if (word.PartOfSpeech == PartOfSpeech.記号) // Symbols
+            {
+                continue;
+            }
+
+            int start = text.IndexOf(word.Word, cursor);
+            int end = start + word.Word.Length;
+
+            ranges.Add(new(start, end));
+
+            cursor = end;
+        }
+
+        return ranges;
+    }
+
+    public void Dispose() => mecab.Dispose();
+}
