bc0e112e42d226c0d724e61d62a989f631e928e5
Max Kagamine <max@kgmn.jp>
2024-08-17 21:25:50 +0900

    Moved QuotesIndex to the main Elasticsearch project

--- Serifu.Data.Elasticsearch/QuotesIndex.cs
+++ Serifu.Data.Elasticsearch/QuotesIndex.cs
@@ -17,11 +17,11 @@ using Elastic.Clients.Elasticsearch.IndexManagement;
 using Elastic.Clients.Elasticsearch.Mapping;
 using System.Text.Json.Serialization;
 
-namespace Serifu.Data.Elasticsearch.Build;
+namespace Serifu.Data.Elasticsearch;
 
-internal static class QuotesIndex
+public static class QuotesIndex
 {
-    private const string IndexName = "quotes";
+    public const string Name = "quotes";
 
     private const string EnglishConjugationsAnalyzer = "english"; // Built-in
     private const string JapaneseConjugationsAnalyzer = "serifu_japanese_conjugations";
@@ -31,13 +31,7 @@ internal static class QuotesIndex
     private const string CjkFilter = "serifu_cjk_filter";
     private const string DesDivFilter = "serifu_desdiv_filter";
 
-    public static CreateIndexRequest Create() => new(IndexName)
-    {
-        Mappings = CreateMappings(),
-        Settings = CreateSettings(),
-    };
-
-    private static TypeMapping CreateMappings() => new()
+    public static TypeMapping Mappings => new()
     {
         Dynamic = DynamicMapping.Strict,
         Properties = new()
@@ -55,6 +49,14 @@ internal static class QuotesIndex
                 Index = false
             }
         }
+    };
+
+    public static IndexSettings Settings => new()
+    {
+        Analysis = CreateAnalysisSettings(),
+        NumberOfShards = 1,
+        NumberOfReplicas = 0,
+        RefreshInterval = -1
     };
 
     private static ObjectProperty CreateTranslationMappings(string conjugationsAnalyzer) => new()
@@ -92,14 +94,6 @@ internal static class QuotesIndex
                 Index = false
             }
         }
-    };
-
-    private static IndexSettings CreateSettings() => new()
-    {
-        Analysis = CreateAnalysisSettings(),
-        NumberOfShards = 1,
-        NumberOfReplicas = 0,
-        RefreshInterval = -1
     };
 
     private static IndexSettingsAnalysis CreateAnalysisSettings() => new()
