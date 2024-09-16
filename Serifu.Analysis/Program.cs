1d9490267db720ca95a642031464369287c2e808
Max Kagamine <max@kgmn.jp>
2024-09-16 22:49:05 +0900

    Iterate over quotes & calculate word frequencies

--- Serifu.Analysis/Program.cs
+++ Serifu.Analysis/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Kagamine.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using Serifu.Analysis;
 using Serifu.Analysis.WordExtractors;
+using Serifu.Data;
 using Serifu.Data.Sqlite;
 using Serilog;
 using Spectre.Console;
@@ -17,6 +20,7 @@ builder.Services.AddSerifuSqlite();
 
 builder.Run(async (
     ISqliteService sqliteService,
+    SerifuDbContext db,
     ILogger logger,
     CancellationToken cancellationToken
 ) =>
@@ -28,9 +32,57 @@ builder.Run(async (
         .Invoke();
 
     [DisplayName("Run analysis")]
-    Task RunAnalysis()
+    async Task RunAnalysis()
     {
-        throw new NotImplementedException();
+        IReadOnlyDictionary<Source, IReadOnlyList<VocabWord>> englishVocab = null!;
+        IReadOnlyDictionary<Source, IReadOnlyList<VocabWord>> japaneseVocab = null!;
+
+        // Not strictly accurate as ~250 will be removed by GetQuotesForExport, but good enough for a progress bar and
+        // avoids having to load all of the quotes into memory first
+        int approxCount = await db.Quotes.CountAsync(cancellationToken);
+        await AnsiConsole.Progress().StartAsync(async ctx =>
+        {
+            var task1 = ctx.AddTask("Indexing words", maxValue: approxCount);
+
+            VocabAnalyzer englishVocabAnalyzer = new(new EnglishWordExtractor(), q => q.English);
+            VocabAnalyzer japaneseVocabAnalyzer = new(new JapaneseWordExtractor(), q => q.Japanese);
+
+            await foreach (Quote quote in sqliteService.GetQuotesForExport(cancellationToken))
+            {
+                englishVocabAnalyzer.Pipe(quote);
+                japaneseVocabAnalyzer.Pipe(quote);
+
+                task1.Increment(1);
+            }
+
+            task1.Value = approxCount;
+            var task2 = ctx.AddTask("Analyzing");
+            task2.IsIndeterminate = true;
+
+            englishVocab = englishVocabAnalyzer.ExtractVocab();
+            japaneseVocab = japaneseVocabAnalyzer.ExtractVocab();
+
+            task2.IsIndeterminate = false;
+            task2.Value = 100;
+        });
+
+        foreach (Source source in englishVocab.Keys)
+        {
+            static Table CreateTable(IReadOnlyList<VocabWord> vocab)
+            {
+                var table = new Table().AddColumns("Word", "Count", "Total count", "Score");
+
+                foreach (var word in vocab)
+                {
+                    table.AddRow([word.Word, word.Count.ToString(), word.TotalCount.ToString(), word.Score.ToString("f4")]);
+                }
+
+                return table;
+            }
+
+            AnsiConsole.Write(new Rule(source.ToString()));
+            AnsiConsole.Write(new Columns(CreateTable(englishVocab[source]), CreateTable(japaneseVocab[source])));
+        }
     }
 
     [DisplayName("Test word extractors")]
---
+++ Serifu.Analysis/VocabAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using Serifu.Analysis.WordExtractors;
+using Serifu.Data;
+
+namespace Serifu.Analysis;
+
+internal record VocabWord(string Word, int Count, int TotalCount, double Score);
+
+internal class VocabAnalyzer
+{
+    private readonly IWordExtractor wordExtractor;
+    private readonly Func<Quote, Translation> translationSelector;
+    private readonly WordFrequenciesBySource wordFrequenciesBySource = [];
+
+    public VocabAnalyzer(IWordExtractor wordExtractor, Func<Quote, Translation> translationSelector)
+    {
+        this.wordExtractor = wordExtractor;
+        this.translationSelector = translationSelector;
+    }
+
+    public void Pipe(Quote quote)
+    {
+        string text = translationSelector(quote).Text;
+
+        foreach (var (word, stemmed) in wordExtractor.ExtractWords(text))
+        {
+            wordFrequenciesBySource.Add(quote.Source, word, stemmed);
+        }
+    }
+
+    public IReadOnlyDictionary<Source, IReadOnlyList<VocabWord>> ExtractVocab()
+    {
+        return new Dictionary<Source, IReadOnlyList<VocabWord>>();
+    }
+}
