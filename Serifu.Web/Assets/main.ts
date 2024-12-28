8f98eeae1a17387eab49cb39ef16f41786e239fd
Max Kagamine <max@kgmn.jp>
2024-12-29 06:08:42 +0900

    Remember user's preferred language

--- Serifu.Web/Assets/main.ts
+++ Serifu.Web/Assets/main.ts
@@ -10,3 +10,22 @@ import './images/serifu.svg';
 
 import './search-box';
 import './audio-button';
+
+// Initially, when a user visits the root they'll be redirected to the homepage
+// corresponding to their Accept-Language header. If they switch languages
+// manually, we'll want to remember and redirect them to their preferred
+// language instead next time.
+//
+// Since cookies, unlike localStorage, have a max-age of 400 days[0], it's
+// necessary to "refresh" cookies periodically. The easiest way to do this here
+// is to simply set the culture cookie to the current culture on page load,
+// meaning Accept-Language will only be used the first time and every subsequent
+// visit to the site root will redirect to the last-used language.
+//
+// I'm setting the cookie in JS rather than via headers to avoid problems with
+// CDNs later.
+//
+// [0]: https://developer.chrome.com/blog/cookie-max-age-expires
+//
+const lang = document.documentElement.lang;
+document.cookie = `.AspNetCore.Culture=c=${lang}|uic=${lang}; Max-Age=34560000`;
